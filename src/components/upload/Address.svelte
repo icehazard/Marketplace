7bfcc040cd349c3751cf13c268c463191dae60f0
dr3 <dr3@dr3.com>
2022-06-28 22:41:50 +0700

    Add reverse geocoder for maps

--- src/components/upload/Address.svelte
+++ src/components/upload/Address.svelte
@@ -36,11 +36,18 @@
         $active++;
     }
 
+    let canContinue = false
+
 
     onMount(async() => {
         const loader = new Loader('AIzaSyAmlMcGgumMqP6T8YLFnsQT_tbL4j5wF0s', {libraries: ['places']});
 
         const google = await loader.load();
+
+
+        const geocoder = new google.maps.Geocoder();
+        const infowindow = new google.maps.InfoWindow();
+
         const map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: -34.397, lng: 150.644},
             zoom: 8,
@@ -55,27 +62,66 @@
         });
 
         map.addListener("click", (event) => {
-            console.log(event.latLng.lat())
-            console.log(event.latLng.lng())
+            console.log('Clicked', event.latLng.lat(), event.latLng.lng())
             console.log(event)
             marker.setPosition(event.latLng)
+            let geo = geocodeLatLng(geocoder, map, infowindow, event.latLng.lat(), event.latLng.lng())
+
         })
 
         marker.addListener('dragend', function(event) {
             //marker.setPosition(event.latLng)
             console.log(event.latLng)
+            let geo = geocodeLatLng(geocoder, map, infowindow, event.latLng.lat(), event.latLng.lng())
+
         })
 
         let input = document.getElementById("address");
 
         let ac = new google.maps.places.Autocomplete(input, {componentRestrictions: { country: "th" }})
+
+        // when user clicks suggestion
         ac.addListener("place_changed", (event) => {
             //console.log(ac.getPlace())
             let p = ac.getPlace()
             map.setCenter({lat: p.geometry.location.lat(), lng: p.geometry.location.lng()})
-            map.setZoom(25)
+            map.setZoom(18)
             marker.setPosition({lat: p.geometry.location.lat(), lng: p.geometry.location.lng()})
+            canContinue = true
+
+            let geo = geocodeLatLng(geocoder, map, infowindow, p.geometry.location.lat(), p.geometry.location.lng())
         })
+
+        function geocodeLatLng(
+            geocoder,
+            map,
+            infowindow, lat, lng
+        ) {
+
+            console.log("GEOCODING", lat, lng)
+            const latlng = {
+                lat: lat,
+                lng: lng,
+            };
+
+            geocoder
+                .geocode({ location: latlng})
+                .then((response) => {
+                    if (response.results[0]) {
+                        //map.setZoom(25);
+
+                        marker.setPosition(latlng)
+
+                        console.log(response.results[0])
+                        let c = response.results[0].address_components
+                        infowindow.setContent(`${c[0].long_name}, ${c[1].long_name}, ${c[2].long_name}`);
+                        infowindow.open(map, marker);
+                    } else {
+                        window.alert("No results found");
+                    }
+                })
+                .catch((e) => window.alert("Geocoder failed due to: " + e));
+        }
     })
 
     function searchAddress() {
@@ -87,7 +133,8 @@
     <div class="center col w-sm gap-40">
         <h1 class="font-36 weight-300">Address</h1>
         <p class="text-center">
-            Please provide the Shop Location, so customers can find your store.
+            Please provide the shop location so customers can find your store.
+            Enter your address then drag the marker to the shop's exact location on the map.
         </p>
         <div class=" row grow gap-10 w100">
 <!--            <div class="col grow gap-10">-->
@@ -122,9 +169,10 @@
     </div>
     <br>
     <div id="map" class="center row w90 h-400 gap-40" ></div>
+    <div class="shade4">Left click to place marker (or drag it). Ctrl+scroll to zoom in/out</div>
     <div class="pt-50  row w-sm  w100  z-2">
         <div class=" center w100">
-            <Button on:click={next} type="button" disable={$shopValid[2]} text="CONTINUE" />
+            <Button on:click={next} type="button" disable={!canContinue} text="CONTINUE" />
         </div>
     </div>
 </form>
