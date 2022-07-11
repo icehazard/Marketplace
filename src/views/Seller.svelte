244357e5ec72732c3ae1ccc91673d6d52e32cf78
Sebastian Whitlock <sebastian.whitlock@securcom.me>
2022-07-11 09:59:39 +0700

    multi shops

--- src/views/Seller.svelte
+++ src/views/Seller.svelte
@@ -2,19 +2,21 @@
     import Categories from "comp/toolbars/categories/Categories.svelte";
     import Banner from "comp/seller/Banner.svelte";
     import Reviews from "comp/seller/Reviews.svelte";
-    import Featured from "comp/seller/Featured.svelte";
     import Cover from "comp/seller/Cover.svelte";
-    import AllItems from "comp/seller/AllItems.svelte";
     import Pending from "comp/seller/Pending.svelte";
     import AddListing from "comp/seller/AddListing.svelte";
 
+    import shops from '@/store/shops.js';
     import user from "@/store/user.js";
     import products from "@/store/products.js";
     import { isShopPending, isShopActive } from "@/store/user.js";
-    import { onMount } from "svelte";
+    
+    export let params = {}
 
+    shops.get(params.id)
     user.get();
     products.get();
+
 </script>
 
 <main class="row container my-50 gap-50 grow">
@@ -29,8 +31,6 @@
         {:else if $isShopActive}
             <AddListing />
         {/if}
-        <!-- <Featured />
-        <AllItems /> -->
         <Reviews />
     </section>
 </main>
