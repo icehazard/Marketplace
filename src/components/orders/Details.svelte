114ed1cdefc3a5ece200aa6cd4f72b84c928880d
Sebastian Whitlock <sebastian.whitlock@securcom.me>
2022-07-19 21:56:02 +0700

    expanded

--- src/components/orders/Details.svelte
+++ src/components/orders/Details.svelte
@@ -1,55 +1,13 @@
 <script>
+  import Floating from "./Floating.svelte";
   import Map from "./../atoms/Map.svelte";
-  import Button from "comp/atoms/Button.svelte";
 </script>
 
 <section class="grow col center">
   <div class="w100 col h100 pa-20 gap-20 grow">
     <div class="col gap-10 w100 grow relative">
-      <div class="shade1 pa-20 absolute p-bottom col curve z-3 ma-20">
-        <div class="row space-between pb-20 gap-20" >
-          <button class="border curve pa-10 grow center">
-            Order Details
-          </button>
-          <button class="border curve pa-10 grow center">
-            Payment
-          </button>
-          <button class="border curve pa-10 grow center">
-            Chat
-          </button>
-        </div>
-        <hr>
-        <div class="row space-between align-center h100" >
-          <div class="col gap-20">
-            <span>Order ID</span>
-            <span>#233238723</span>
-          </div>
-          <div class="col gap-20">
-            <span>Product</span>
-            <span>PWhite Widow</span>
-          </div>
-          <div class="col gap-20">
-            <span>Price</span>
-            <span>EUR 289</span>
-          </div>
-          <div class="col gap-20">
-            <span>Address</span>
-            <span>Soi king pattana 4</span>
-          </div>
-        </div>
-      </div>
+      <Floating />
       <Map />
     </div>
-    <!-- <div class="row center">
-        <Button text='Cancel'></Button>
-      </div> -->
   </div>
 </section>
-
-<style>
-  .absolute{
-    bottom: 50px;
-    width: calc(100% - 40px);
-    height: 250px;
-  }
-</style>
---
+++ src/components/orders/Floating.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Chat from './../messages/Chat.svelte';
+    
+    let expand = false;
+
+    function expandBox(){
+        expand = !expand
+    }
+</script>
+
+<div class="shade1 pa-20 absolute p-bottom col curve z-3 ma-20 slow" class:expanded={expand}>
+    <div class="row space-between pb-20 gap-20">
+        <button class="border curve pa-10 grow center"> Order Details </button>
+        <button class="border curve pa-10 grow center"> Payment </button>
+        <button on:click={expandBox} class="border curve pa-10 grow center"> Chat </button>
+    </div>
+    <hr />
+   {#if !expand}
+   <div class="row space-between align-center h100">
+    <div class="col gap-20">
+        <span  class="opacity-75">Order ID</span>
+        <span>#233238723</span>
+    </div>
+    <div class="col gap-20">
+        <span  class="opacity-75">Product</span>
+        <span>PWhite Widow</span>
+    </div>
+    <div class="col gap-20">
+        <span  class="opacity-75">Price</span>
+        <span>EUR 289</span>
+    </div>
+    <div class="col gap-20">
+        <span class="opacity-75">Address</span>
+        <span>Soi king pattana 4</span>
+    </div>
+</div>
+{:else}
+<Chat />
+   {/if}
+</div>
+
+<style>
+    .absolute {
+        bottom: 50px;
+        width: calc(100% - 40px);
+        height: 250px;
+    }
+
+    .expanded{
+        height: calc(100% - 90px);
+    }
+</style>
