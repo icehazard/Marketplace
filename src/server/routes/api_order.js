fc9175b265727e175a2db9776f0ddd60469884c4
dr3 <dr3@dr3.com>
2022-09-01 14:42:31 +0700

    Upd

--- src/server/routes/api_order.js
+++ src/server/routes/api_order.js
@@ -67,15 +67,20 @@ api.post('/', async (req, res) => {
 
         if (!p.qty) {
             errorIds.push(pId);
+            console.log("XXX > cant qty")
         }
 
         let memProd = productHandler.Products.get(pId)
 
-        if (pId != shopId)
+        if (pId != shopId) {
+            console.log("XXX > shop no match")
             errorIds.push(pId);
+        }
 
-        if (p.qty > memProd.qty)
+        if (p.qty > memProd.qty) {
             errorIds.push(pId);
+            console.log("XXX > qty higher than mem")
+        }
         else
         {
             memProd.qty -= p.qty;
