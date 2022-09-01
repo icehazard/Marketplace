c79e134d9b169514dcc79b03a43361a684418b91
Sebastian Whitlock <sebastian.whitlock@securcom.me>
2022-09-01 18:29:57 +0700

    prevent

--- src/components/orders/Chat.svelte
+++ src/components/orders/Chat.svelte
@@ -1,35 +1,30 @@
 <script>
   import Bubble from "./Bubble.svelte";
 
-  let data = [
-    { sender: true, text: "Did you eat anything cocksucker", time: "17:54" },
-  ];
+  let data = [];
+  let msg = "";
 
-  let msg = '';
-
-  function submit(){
-    console.log(msg)
-    data = [...data, { sender: true, text: msg, time: "17:54" }]
-
-    msg = '';
+  function submit() {
+    console.log(msg);
+    data = [...data, { sender: true, text: msg, time: "17:54" }];
+    msg = "";
   }
 </script>
 
 <div class="grow col">
   <div class="col grow frame">
     {#each data as item}
-      <Bubble item={item} />
+      <Bubble {item} />
     {/each}
   </div>
-  <form class="row shade0 px-20  h-60 align-center" on:submit={submit}>
-    <input  bind:value={msg}  type="text" class="w100" placeholder="Write a message" />
+  <form class="row shade0 px-20  h-60 align-center" on:submit|preventDefault={submit}>
+    <input bind:value={msg} type="text" class="w100" placeholder="Write a message" />
   </form>
 </div>
 
 <style>
-  .frame{
+  .frame {
     height: calc(100vh - 190px);
     overflow: overlay;
   }
-  
 </style>
--- src/components/orders/Contact.svelte
+++ src/components/orders/Contact.svelte
@@ -7,6 +7,8 @@
     export let order = {};
     let el;
 
+    $: src = `http://localhost:8080/api/image/` + order.productPhoto;
+
     function view() {
         push(`#/orders/active/${order._id}`);
     }
@@ -16,14 +18,19 @@
     });
 </script>
 
-<button class="row w100 align-center border py-20 curve shine shadow" on:click={view}>
-    <div class="pa-20">
-        <div class="round primary pa-30" />
-    </div>
-    <div class="col grow justify-center gap-15 pr-20">
-        <span>Amnesia Haze</span>
-        <span>{order._id}</span>
-        <span>{order.address}</span>
-        <span bind:this={el} datetime={order.created_at} />
+<button class="row w100 align-center border py-20 curve shine gap-15 pa-5" on:click={view}>
+    <img {src} alt="" class="w-100 h-100 curve" />
+    <div class="col grow justify-center gap-10 pr-20">
+        <span>PRODUCT STATUS</span>
+        <span class="opacity-75">{order._id}</span>
+        <span class="font-12">{order.address}</span>
+        <span class="font-14" bind:this={el} datetime={order.created_at} />
+        <div class="row space-between">
+            <div class="weight-600 font-14">5 items</div>
+            <div>d</div>
+        </div>
     </div>
 </button>
+
+<style>
+</style>
