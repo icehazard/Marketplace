5019ea51ee908d8cd8017c8bb541b7fc49520eb7
Sebastian Whitlock <sebastian.whitlock@securcom.me>
2022-09-03 19:50:28 +0700

    justified chats based on sender

--- src/components/orders/Bubble.svelte
+++ src/components/orders/Bubble.svelte
@@ -1,14 +1,17 @@
 <script>
     export let item;
     import dayjs from 'dayjs'
+    import user from '@/store/user'
+    
+    let me = item.senderId == $user._id
 </script>
 
-<div class="row pa-20 " class:justify-end={item.sender}>
-    <div class="col gap-10" class:align-end={item.sender}>
+<div class="row pa-20 " class:justify-end={me}>
+    <div class="col gap-10" class:align-end={me}>
         <div
             class="pa-20 curve"
-            class:primary={!item.sender}
-            class:shade3={item.sender}
+            class:primary={!me}
+            class:shade3={me}
         >
             {item.msg}
         </div>
